ca4f6038989552625fd98fffa858c1e1027a7d63
CJxD <Chris@CJWatts.com>
2014-12-11 12:14:07 +0000

    Random Guesser

---
+++ src/main/java/uk/ac/soton/ecs/imageclassifer/RandomGuesser.java
@@ -0,0 +1,46 @@
+package uk.ac.soton.ecs.imageclassifer;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.image.FImage;
+import org.openimaj.ml.annotation.Annotated;
+
+/**
+ * 
+ * Literally just guesses classes randomly
+ * 
+ * @author cw17g12
+ *
+ */
+public class RandomGuesser implements ClassificationAlgorithm
+{
+	private HashSet<String> classes = new HashSet<>();
+	
+	@Override
+	public ClassificationResult<String> classify(FImage object)
+	{
+		int clazz = new Random().nextInt(classes.size());
+		
+		PrintableClassificationResult<String> result = new PrintableClassificationResult<>();
+		result.put(classes.toArray(new String[0])[clazz], 1);
+		
+		return result;
+	}
+
+	@Override
+	public void train(List<? extends Annotated<FImage, String>> data)
+	{
+		// Make a set of known classes
+		for (Annotated<FImage, String> d : data) {
+			classes.add(d.getAnnotations().iterator().next());
+		}
+		
+		// Give it enough time to train
+		long finish = System.currentTimeMillis() + 30000;
+		while (System.currentTimeMillis() < finish) {}
+	}
+
+}
--- src/test/java/uk/ac/soton/ecs/imageclassifier/AccuracyTest.java
+++ src/test/java/uk/ac/soton/ecs/imageclassifier/AccuracyTest.java
@@ -14,45 +14,73 @@ import org.openimaj.ml.annotation.AnnotatedObject;
 
 import uk.ac.soton.ecs.imageclassifer.*;
 
+/**
+ * 
+ * Tests classification algorithms on a subset of the training data
+ * 
+ * @author cw17g12
+ *
+ */
 public class AccuracyTest
 {
-	public static void main(String[] args) throws FileSystemException {
-		new AccuracyTest(new SIFTBoVW());
+	public static void main(String[] args) throws FileSystemException
+	{
+		new AccuracyTest(new RandomGuesser(), 10, 10);
 	}
-	
-	public AccuracyTest(ClassificationAlgorithm alg) throws FileSystemException {
+
+	public AccuracyTest(ClassificationAlgorithm alg, int numTraining, int numTesting) throws FileSystemException
+	{
+		System.out.println("Loading Images...");
 		File trainingFile = new File("imagesets/training");
-		
+
 		VFSGroupDataset<FImage> data = new VFSGroupDataset<>(
 			trainingFile.getAbsolutePath(),
 			ImageUtilities.FIMAGE_READER);
-		
-		GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<>(data, 10, 0, 10);
-		
+
+		GroupedRandomSplitter<String, FImage> splits = new GroupedRandomSplitter<>(data, numTraining, 0, numTesting);
+
+		System.out.println("Performing Training...");
 		alg.train(AnnotatedObject.createList(splits.getTrainingDataset()));
-		
+
+		/*
+		 * Iterate over each image in the group dataset and check that the
+		 * classified result is the same as the group name
+		 */
 		int correct = 0;
-		for (Entry<String, ListDataset<FImage>> e : splits.getTestDataset().entrySet()) {
-			for (FImage image : e.getValue()) {
+		for(Entry<String, ListDataset<FImage>> e : splits.getTestDataset().entrySet())
+		{
+			for(FImage image : e.getValue())
+			{
 				ClassificationResult<String> c = alg.classify(image);
-				
+
+				/*
+				 * Iterate over results to find the one with the highest confidence
+				 */
 				double confidence = 0;
 				String mostLikely = "unknown";
-				for (String clazz : c.getPredictedClasses())
+				for(String clazz : c.getPredictedClasses())
 				{
 					double conf = c.getConfidence(clazz);
-					if (conf > confidence) {
+					if(conf > confidence)
+					{
 						mostLikely = clazz;
 						confidence = conf;
 					}
 				}
-				
+
+				/*
+				 * Show expected and classified values
+				 */
 				System.out.printf("Expected: %s, Returned: %s\n",
 					e.getKey(), mostLikely);
-				if (mostLikely.equals(e.getKey())) correct++;
+				if(mostLikely.equals(e.getKey()))
+					correct++;
 			}
 		}
-		
+
+		/*
+		 * Show percentage error
+		 */
 		System.out.printf("Accuracy: %f%%\n", correct * 100f / splits.getTestDataset().numInstances());
 	}
 }
